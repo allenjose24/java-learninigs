6e16c106fdca65335149f7d894ec163791385eb5
allenjose24 <allenjose2110@gmail.com>
2025-12-20 12:54:38 +0530

    feat: Create Java program to read and print user age and name.

---
+++ Day-3/Task1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        // sc.nextLine(); /*to consume the newline character which comes after the digits entered by the user before the newline character*/
+        sc.nextLine();
+        System.out.print("Enter Name: ");
+        String str = sc.nextLine();
+
+        sc.close();
+        
+        System.out.println("Name: " + str);
+        System.out.println("Age: " + age);
+    }
+}
