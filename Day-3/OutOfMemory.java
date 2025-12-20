de620fa054e8e70f634f69bd6d34c640b20e4c43
allenjose24 <allenjose2110@gmail.com>
2025-12-20 12:12:17 +0530

    feat: Add Java example demonstrating OutOfMemoryError.

--- Day-3/OutOfMemory.java
+++ Day-3/OutOfMemory.java
@@ -3,8 +3,18 @@ class OutOfMemory{
 	double did;
 	double salary;
 	long uid;
+
+	/*
+	WAP to show the difference between OutOfMemoryError and StackOverflowError
+	*/
 	public static void main(String[] args){
 		OutOfMemory arr[] = new OutOfMemory[1000000000];
 		System.out.println("Working");
+
+		try {
+			OutOfMemory arr2[] = new OutOfMemory[1000000000];
+		} catch (OutOfMemoryError e) {
+			System.out.println(e);
+		}
 	}
 }
