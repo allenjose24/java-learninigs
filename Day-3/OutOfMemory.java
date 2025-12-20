9d2e9494cca7812fa386e447c6c7c790faa576cf
allenjose24 <allenjose2110@gmail.com>
2025-12-20 12:09:45 +0530

    feat: Add Java examples for OutOfMemoryError and type casting/conversion.

---
+++ Day-3/OutOfMemory.java
@@ -0,0 +1,10 @@
+class OutOfMemory{
+	long id;
+	double did;
+	double salary;
+	long uid;
+	public static void main(String[] args){
+		OutOfMemory arr[] = new OutOfMemory[1000000000];
+		System.out.println("Working");
+	}
+}
