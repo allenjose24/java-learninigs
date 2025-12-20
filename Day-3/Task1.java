596917ebe878bababd98b0bb063eacae80d2a9da
allenjose24 <allenjose2110@gmail.com>
2025-12-20 15:29:21 +0530

    feat: add new Java programs for basic input/output, arithmetic operations, and conditional checks.

--- Day-3/Task1.java
+++ Day-3/Task1.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class Task1 {
+    /*
+     * WAP to take age and name as input from the user and print it
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter age: ");
