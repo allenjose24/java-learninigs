f3b87fcab6991e1f7d050cecb53ce4c98835c491
allenjose24 <allenjose2110@gmail.com>
2025-12-20 10:13:37 +0530

    Allowed to take space as an input

--- Day-3/TernaryOp.java
+++ Day-3/TernaryOp.java
@@ -1,13 +1,13 @@
-import module java.base;
+import java.util.Scanner;
 
 public class TernaryOp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char ch = sc.next().charAt(0);
-	sc.nextLine();
-	sc.close();
-        int ascii = ch;
-        String answer = (ch >= 65 && ch<= 90 || ch>=97 && ch<=122 ) ? "Alphabet" : (ch >= 48 && ch<= 57) ? "Number" : "Special Characters";
-	System.out.println(answer);  
+        String str = sc.nextLine();
+	    char ch = str.charAt(0);
+	    int ascii = ch;
+	    sc.close();
+        String answer = (ascii >= 65 && ascii<= 90 || ascii>=97 && ascii<=122 ) ? "Alphabet" : (ascii >= 48 && ascii<= 57) ? "Number" : "Special Characters";
+	    System.out.println(answer);  
     }
 }
