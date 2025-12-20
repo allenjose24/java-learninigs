e9e21f8bf3e15ba975cc11b98bb93e922a88fd4c
allenjose24 <allenjose2110@gmail.com>
2025-12-20 14:46:03 +0530

    feat: Introduce Java programs for user input collection and job eligibility determination.

---
+++ Day-3/JobEligibility.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class JobEligibility {
+    /*
+    Degree btech
+    Stream It
+    knowledege Java */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name  = sc.nextLine();
+        System.out.println("Enter your stream: ");
+        String stream = sc.nextLine();
+        System.out.println("Enter your percentage: ");
+        double percentage = sc.nextDouble();
+        sc.close();
+        
+    }
+}
