1bbc78297c4fd9de49a20b2cc8c75ae445652603
allenjose24 <allenjose2110@gmail.com>
2025-12-22 12:46:08 +0530

    feat: add basic Java examples demonstrating `while` loop usage

---
+++ Day-4/LoopingStat.java
@@ -0,0 +1,11 @@
+public class LoopingStat {
+    /*
+    Pattern printing problems using looping statements */
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=5){
+            System.out.print("* ");
+            i++;
+        }
+    }
+}
---
+++ Day-4/WhileStat.java
@@ -0,0 +1,12 @@
+public class WhileStat {
+    public static void main(String[] args) {
+        System.out.println("-----Software starts-------");
+        int i = 1;
+        while(i<=5){
+            System.out.println("Hi");
+            System.out.println("Hello");
+            i++;
+        }
+        System.out.println("-----Software closed-------");
+    }
+}
