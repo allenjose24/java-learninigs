fc171b30e27e50874046600f483356e598ae9376
allenjose24 <allenjose2110@gmail.com>
2025-12-22 11:30:19 +0530

    feat: Add Java solutions for Day 4 tasks covering conditional logic and user input.

--- Day-4/Task1.java
+++ Day-4/Task1.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Task1 {
+    /*
+    Check 
+    - if age is above 20 then he can drive a car
+    - if age is above 18 and below 20 then he can ride a bike
+    - if age is above 16 and below 18 then he can ride a scooty
+    - if age is below 16 then he can't ride anything */
 
     public static void main(String[] args) {
         
