ef0f799883aa36dfe4702920c75a8e610e816fd0
allenjose24 <allenjose2110@gmail.com>
2025-12-22 11:30:47 +0530

    feat: Add Java exercises demonstrating conditional logic, user input, and basic calculations.

--- Day-4/Task3.java
+++ Day-4/Task3.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Task3 {
+    /*
+    Ask user to enter day and print the corresponding meal */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String day = scanner.nextLine();
