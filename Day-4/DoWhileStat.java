2482a36ed45985d1486979b4a2c7b123258ae49c
allenjose24 <allenjose2110@gmail.com>
2025-12-22 12:45:32 +0530

    feat: Add Java examples for `while`, `do-while`, and `for` looping statements, including factorial calculation.

---
+++ Day-4/DoWhileStat.java
@@ -0,0 +1,13 @@
+public class DoWhileStat {
+    public static void main(String[] args) {
+        System.out.println("-----Software starts-------");
+        int i = 1;
+        do{
+            System.out.println("Hi");
+            System.out.println("Hello");
+            i++;
+        }while(i<=5);
+        System.out.println("-----Software closed-------");
+         
+    }
+}
