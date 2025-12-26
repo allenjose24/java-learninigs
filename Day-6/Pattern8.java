be1880c2cdc7163a1d045aed21a4e8cdddbf2931
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:51:17 +0530

    feat: Add new Java files for various pattern printing and basic array/number manipulation tasks.

---
+++ Day-6/Pattern8.java
@@ -0,0 +1,10 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        for(int i=90;i>=85;i--){
+            for(int j=1;j<=5;j++){
+                System.out.print((char)(i)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
