4680537f1e02a169540ba59db73bbfe0febd9bf7
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:51:08 +0530

    feat: Add various pattern printing examples and basic array/number manipulation tasks.

---
+++ Day-6/Pattern7.java
@@ -0,0 +1,10 @@
+public class Pattern7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=65;j<=70;j++){
+                System.out.print((char)(j)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
