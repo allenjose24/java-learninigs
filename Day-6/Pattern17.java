b56fea784a2e9455916f1506230bc7c155fec420
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:53:16 +0530

    feat: Implement Java programs for array identity/reversal, palindrome checks, and star pattern printing.

---
+++ Day-6/Pattern17.java
@@ -0,0 +1,13 @@
+public class Pattern17 {
+    public static void main(String[] args) {
+        for(int i=1,j=5;i<=j && j>=1;){
+            System.out.print("* ");
+            if(i==j){
+                j--;
+                System.out.println();
+                i=1;
+            }
+            else i++;
+        }
+    }
+}
