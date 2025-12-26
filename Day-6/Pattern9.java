a1f783b94bd5a4ae4dd608ece8662a2bb0cf8306
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:51:33 +0530

    feat: Add various Java pattern printing and array/number manipulation exercises.

---
+++ Day-6/Pattern9.java
@@ -0,0 +1,11 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        char ch = 'A';
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print(ch++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
