266468792e25f0fb695bc2fd4cccad264e71cd64
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:50:45 +0530

    feat: Add Java programs for various pattern printing exercises and array manipulation tasks.

---
+++ Day-6/Pattern5.java
@@ -0,0 +1,10 @@
+public class Pattern5 {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=1; j<=5; j++){
+                System.out.print(i*5+j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
