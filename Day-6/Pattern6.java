bac82528f14aa475cc6ceb9d004b1e7ded43c13f
allenjose24 <allenjose2110@gmail.com>
2025-12-26 08:50:56 +0530

    feat: Add new Java files for various tasks and pattern printing exercises.

---
+++ Day-6/Pattern6.java
@@ -0,0 +1,11 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(k++%10+" ");
+            }
+            System.out.println();
+        }
+    }
+}
